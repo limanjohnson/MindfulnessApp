f5f1c21d45299a37e659ce7c8fa527dd04448cb6
Liam <liam@simplifybiz.com>
2025-04-08 20:14:13 -0600

    LN Mobile screen optimization

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import { loadHeaderFooter, mobileMenuToggle } from "./utils.mjs"
+import { navigationMenu } from "./navigation.mjs"
 
 document.addEventListener("DOMContentLoaded", () => {
   loadHeaderFooter().then(() => {
     mobileMenuToggle();
+    navigationMenu();
   });
 })
 
---
+++ src/js/navigation.mjs
@@ -0,0 +1,10 @@
+
+export function navigationMenu() {
+  const navigationMenuElement = document.querySelector("#navigationMenu");
+
+  navigationMenuElement.innerHTML = `
+  <a href="../index.html">Dashboard</a>
+  <a href="#">Activites</a>
+  <a href="#">Profile</a>
+  `;
+}
