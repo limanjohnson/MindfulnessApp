e1640cd1c7b6b85ab04493685d2e1e16448f03d9
Liam <liam@simplifybiz.com>
2025-04-08 20:16:27 -0600

    LN Mobile screen optimization

--- src/js/navigation.mjs
+++ src/js/navigation.mjs
@@ -3,7 +3,7 @@ export function navigationMenu() {
   const navigationMenuElement = document.querySelector("#navigationMenu");
 
   navigationMenuElement.innerHTML = `
-  <a href="../index.html">Dashboard</a>
+  <a href="index.html">Dashboard</a>
   <a href="#">Activites</a>
   <a href="#">Profile</a>
   `;
